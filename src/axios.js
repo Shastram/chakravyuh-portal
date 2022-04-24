a148afdf6c947a279378a9c38ee3a22978a1fe82
AryakRoy <aryakroy63@gmail.com>
2022-04-25 02:11:52 +0530

    Corrected ENV Variable

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "http://20.124.13.106:8000",
+    baseURL: process.env.API_URL,
     headers: {
         "content-type": "application/json"
     }
